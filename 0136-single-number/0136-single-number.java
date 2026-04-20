53b1d607f3a869ebd26691e768212cf2b8dbd56e
Sumit <adeppasumit4@gmail.com>
2026-04-20 18:55:04 +0530

    Time: 1 ms (99.98%), Space: 47.2 MB (17.49%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,14 +1,9 @@
 class Solution {
     public int singleNumber(int[] arr) {
-
-        int n = arr.length;
         int sum = 0;
-
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < arr.length; i++) {
            sum ^= arr[i];
         }
-
         return sum;
-        
     }
 }
