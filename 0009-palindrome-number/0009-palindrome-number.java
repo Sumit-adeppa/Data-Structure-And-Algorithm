4e01de72b2fe5a6e1aff9d061d858fd23cf646fc
Sumit <adeppasumit4@gmail.com>
2026-05-02 12:38:27 +0530

    Time: 5 ms (79.65%), Space: 46.1 MB (34.33%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -15,3 +15,28 @@ class Solution {
         return temp == rev;
     }
 }
+/*
+    x = 121
+    x = 121, so the negative check passes.
+
+    temp = 121, rev = 0
+
+    Iteration 1:
+    digit = 121 % 10 = 1
+    rev   = 0 * 10 + 1 = 1
+    x     = 121 / 10 = 12
+
+    Iteration 2:
+    digit = 12 % 10 = 2
+    rev   = 1 * 10 + 2 = 12
+    x     = 12 / 10 = 1
+
+    Iteration 3:
+    digit = 1 % 10 = 1
+    rev   = 12 * 10 + 1 = 121
+    x     = 1 / 10 = 0
+
+    x = 0, exit loop.
+    
+    return temp == rev => 121 == 121 => true. Correct.
+*/
