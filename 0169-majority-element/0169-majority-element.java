42421382363e5e999d95ad4dc2c5abd36c085d90
Sumit <adeppasumit4@gmail.com>
2026-05-02 16:03:36 +0530

    Time: 2 ms (70.91%), Space: 55.5 MB (66.92%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -10,3 +10,15 @@ class Solution {
         return candidate;
     }
 }
+
+/* 
+    Dry run on [2, 2, 1, 1, 1, 2, 2]:
+    Index 0: num=2 | count=0, so candidate=2, count=1
+    Index 1: num=2 | num==candidate, count=2
+    Index 2: num=1 | num!=candidate, count=1
+    Index 3: num=1 | num!=candidate, count=0
+    Index 4: num=1 | count=0, so candidate=1, count=1
+    Index 5: num=2 | num!=candidate, count=0
+    Index 6: num=2 | count=0, so candidate=2, count=1
+    Final candidate = 2. Correct.
+*/
