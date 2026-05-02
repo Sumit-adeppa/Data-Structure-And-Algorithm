7e16ef07560f20249e06615d7c4a72bc28d808a7
Sumit <adeppasumit4@gmail.com>
2026-05-02 12:35:16 +0530

    Time: 1 ms (99.96%), Space: 42.7 MB (45.35%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -22,3 +22,37 @@ class Solution {
 
     }
 }
+
+/*
+    Key constants to know before starting:
+    Integer.MAX_VALUE     = 2147483647
+    Integer.MAX_VALUE/10  = 214748364   (last safe digit: 7)
+    Integer.MIN_VALUE     = -2147483648
+    Integer.MIN_VALUE/10  = -214748364  (last safe digit: -8)
+
+    Example 1: x = 123
+
+    temp = 123, 
+    rev = 0
+
+    Iteration 1:
+    digit = 123 % 10 = 3
+    temp  = 123 / 10 = 12
+    overflow check: 0 > 214748364? No. 0 < -214748364? No.
+    rev = 0 * 10 + 3 = 3
+
+    Iteration 2:
+    digit = 12 % 10 = 2
+    temp  = 12 / 10  = 1
+    overflow check: 3 > 214748364? No. 3 < -214748364? No.
+    rev = 3 * 10 + 2 = 32
+
+    Iteration 3:
+    digit = 1 % 10 = 1
+    temp  = 1 / 10  = 0
+    overflow check: 32 > 214748364? No. 32 < -214748364? No.
+    rev = 32 * 10 + 1 = 321
+    
+    temp = 0, exit loop.
+    return 321. Correct.
+*/
