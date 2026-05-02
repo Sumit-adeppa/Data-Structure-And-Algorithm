a2683041ede5f68e86c743e8abadea02511f2a79
Sumit <adeppasumit4@gmail.com>
2026-05-02 16:07:56 +0530

    Time: 0 ms (100%), Space: 43.5 MB (61.26%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -16,3 +16,14 @@ class Solution {
 
     }
 }
+
+/*
+    Dry run on [3, 2, 2, 3], val=3:
+    k=0, i=0 | nums[0]=3 == val  | skip       | k=0
+    k=0, i=1 | nums[1]=2 != val  | nums[0]=2  | k=1
+    k=1, i=2 | nums[2]=2 != val  | nums[1]=2  | k=2
+    k=2, i=3 | nums[3]=3 == val  | skip       | k=2
+
+    Array after: [2, 2, _, _]
+    Return k=2 ✓ 
+*/
