68b7567fc6d498153ff3f946521d03139fa2038b
Sumit <adeppasumit4@gmail.com>
2026-05-02 16:14:23 +0530

    Time: 2 ms (39.18%), Space: 94.4 MB (69.76%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,20 +1,6 @@
 class Solution {
     public int maxProfit(int[] prices) {
 
-        /*
-            prices = [7, 1, 5, 3, 6, 4]
-            Start: buyPrice = 7, max_Profit = 0
-            i=1: price=1, cur_Profit = 1-7 = -6,  max_Profit=0,  buyPrice=1
-            i=2: price=5, cur_Profit = 5-1 =  4,  max_Profit=4,  buyPrice=1
-            i=3: price=3, cur_Profit = 3-1 =  2,  max_Profit=4,  buyPrice=1
-            i=4: price=6, cur_Profit = 6-1 =  5,  max_Profit=5,  buyPrice=1
-            i=5: price=4, cur_Profit = 4-1 =  3,  max_Profit=5,  buyPrice=1
-            Answer: 5
-
-            Two rules per step:
-                1. If cur_Profit beats max_Profit, update max_Profit.
-                2. If today's price is lower than buyPrice, update buyPrice.
-        */
 
         int n = prices.length;
         int max_Profit = 0;
@@ -33,3 +19,17 @@ class Solution {
         return max_Profit;
     }
 }
+/*
+    prices = [7, 1, 5, 3, 6, 4]
+    Start: buyPrice = 7, max_Profit = 0
+    i=1: price=1, cur_Profit = 1-7 = -6,  max_Profit=0,  buyPrice=1
+    i=2: price=5, cur_Profit = 5-1 =  4,  max_Profit=4,  buyPrice=1
+    i=3: price=3, cur_Profit = 3-1 =  2,  max_Profit=4,  buyPrice=1
+    i=4: price=6, cur_Profit = 6-1 =  5,  max_Profit=5,  buyPrice=1
+    i=5: price=4, cur_Profit = 4-1 =  3,  max_Profit=5,  buyPrice=1
+    Answer: 5
+
+    Two rules per step:
+        1. If cur_Profit beats max_Profit, update max_Profit.
+        2. If today's price is lower than buyPrice, update buyPrice.
+*/
